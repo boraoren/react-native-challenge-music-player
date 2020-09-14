9de7df187942d134c08083d2fae0295f87ec758e
bora <boraoren@gmail.com>
2020-09-15 09:11:30 +1000

    feat(screen1/header/menu): implement menu with background color

--- src/components/screen/header/index.tsx
+++ src/components/screen/header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import ScreenHeaderMenu from './menu';
 
 const ScreenHeader = () => {
     return(
         <View>
             <Text>
-                Screen Header
+                <ScreenHeaderMenu/>
             </Text>
         </View>
     )
---
+++ src/components/screen/header/menu/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ScreenHeaderMenu = () => {
+    return(
+        <View style={styles.menu}>
+           <Text style={[styles.menuLine,{marginLeft: 35}]}>__</Text>
+           <Text style={[styles.menuLine,{marginLeft: 50}]}>__</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    menu: {
+        paddingTop: 25,
+        backgroundColor: '#E7E84E'
+    },
+    menuLine: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        lineHeight: 10,
+    }
+})
+
+export default ScreenHeaderMenu;
