57192415742b0679cfb4aa0b9310dded851c604e
bora <boraoren@gmail.com>
2020-09-15 09:48:50 +1000

    feat(screen1/singer/image): implement singer image and centeralized singer name

--- src/components/singer/index.tsx
+++ src/components/singer/index.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const singerImage = require('./singerImage.png')
 
 const Singer = () => {
     return (
-        <View>
-            <Text style={styles.name}>
-                NLE Choppa
-            </Text>
+        <View style={styles.singer}>
+            <ImageBackground 
+                source={singerImage}
+                style={styles.singerImageBackground}
+                borderBottomRightRadius={50}>
+                <Text style={styles.name}>NLE Choppa</Text>
+            </ImageBackground>
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    singer: {
+        flex: 1,
+    },
     name: {
         fontSize: 24,
         marginTop: 54,
         fontWeight: 'bold',
-        fontFamily: 'Gilroy'
+        fontFamily: 'Gilroy',
+        textAlign: 'center',
+    },
+    singerImageBackground: {
+        width: '100%',
+        height: 500,
+        shadowColor: '#000',
+        shadowOffset: {width: 1, height: 5},
+        shadowOpacity: 0.5,
+        shadowRadius: 3,
     }
 })
 
