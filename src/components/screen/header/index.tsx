f35222a4b1d72dc43f47a12598ee91800910997b
bora <boraoren@gmail.com>
2020-09-15 08:53:46 +1000

    feat(screen/header): implement draft

---
+++ src/components/screen/header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const ScreenHeader = () => {
+    return(
+        <View>
+            <Text>
+                Screen Header
+            </Text>
+        </View>
+    )
+}
+
+export default ScreenHeader;
--- src/screens/Screen1.tsx
+++ src/screens/Screen1.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ScreenHeader from '../components/screen/header';
 
 const Screen1 = () => {
     return(
         <SafeAreaView>
             <Text style={styles.text}>
-                Screen 1
+                <ScreenHeader/>
             </Text>
         </SafeAreaView>
     )
