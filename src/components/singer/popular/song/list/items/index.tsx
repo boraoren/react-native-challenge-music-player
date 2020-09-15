fe83fd986acd3fcd12cf9d30dd3d2b07813f66f8
bora <boraoren@gmail.com>
2020-09-15 11:17:15 +1000

    feat(screen1/singer/popular/song/list/items/item): implement item components

--- src/components/singer/popular/song/list/items/index.tsx
+++ src/components/singer/popular/song/list/items/index.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import SingerPopularSongListItemsItem, { SingerPopularSongListItemsItemProps } from './item';
 
 const SingerPopularSongListItems = () => {
+
+    const songItems = [
+        {
+            id:1,
+            title: 'Camelot',
+            isPlaying: true,
+            totalTime: '3:00',
+            remainingTime: '00:28',
+            isLiked: false,
+        },
+        {
+            id: 2,
+            title: 'Shotta Flow',
+            isPlaying: false,
+            totalTime: '02:40',
+            remainingTime: '0',
+            isLiked: true,
+        },
+        {
+            id: 3,
+            title: 'Sunrise',
+            isPlaying: false,
+            totalTime: '3:20',
+            remainingTime: '0',
+            isLiked: false,
+        }
+    ] as SingerPopularSongListItemsItemProps[];
+
     return(
-        <View>
-            <Text>
-                Singer Popular Song List Items
-            </Text>
+        <View style={styles.singerPopularListItems}>
+            <FlatList
+                data= {songItems}
+                renderItem={SingerPopularSongListItemsItem}
+                keyExtractor={(item)=> item.id.toString()}
+            />            
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    singerPopularListItems: {
+        flex: 1,
+    }
+})
 
 export default SingerPopularSongListItems;
---
+++ src/components/singer/popular/song/list/items/item/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export interface SingerPopularSongListItemsItemProps {
+    id:number;
+    title: string;
+    isPlaying: boolean;
+    totalTime: string;
+    remainingTime: string;
+    isLiked: boolean;
+}
+
+const SingerPopularSongListItemsItem = ({
+    item
+}:{
+    item:SingerPopularSongListItemsItemProps
+}) =>{
+    return(
+        <View
+            style={styles().singerPopularSongListItemsItem}>
+
+            <Text>{item.isPlaying? 'PLAY  ':'PAUSE'}</Text>
+
+            <View style={styles().titlePanel}>
+                <Text style={styles(item).title}>{item.title}</Text>
+            </View>
+
+            <Text style={styles(item).timing}>
+                {item.isPlaying ? item.remainingTime : item.totalTime}
+            </Text>
+            <Text style={styles(item).like}>&hearts;</Text>
+        </View>
+    )
+}
+
+const styles = (item?: SingerPopularSongListItemsItemProps) => StyleSheet.create({
+
+    singerPopularSongListItemsItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 40,
+    },
+    titlePanel: {
+        flex: 1,
+        marginLeft: 30,
+        justifyContent: 'center',
+    },
+    title: {
+        color: item?.isPlaying ? '#000' : '#CCCDCF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    timing: {
+        alignSelf: 'center',
+        color: item?.isPlaying ? '#000' : '#CCCDCF',
+    },
+    like: {
+        alignSelf: 'center',
+        color: item?.isLiked ? '#000' : '#CCCDCF',
+        fontSize: 30,
+        marginLeft: 35,
+    }
+})
+
+export default SingerPopularSongListItemsItem;
